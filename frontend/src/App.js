671bc7ef04fbe09677a31544b717a01fa60f23e6
akash rattan <akashrattan43@Gmail.com>
2020-12-01 21:00:01 -0800

    products added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,20 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
+
 const App = () => {
   return (
-    <div>
-      <h1>Shop</h1>
-    </div>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
