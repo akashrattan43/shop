753d41fd5057ba2c124b55a85fc741b13a7de5ca
akash rattan <akashrattan43@Gmail.com>
2020-12-10 13:21:31 -0800

    modal created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
+import colors from 'colors'
 import connectDB from "./config/db.js";
 
 dotenv.config();
@@ -27,6 +28,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
