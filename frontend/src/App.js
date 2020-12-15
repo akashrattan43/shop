4fb1a340674bb684a9175d587f056379cb6d9381
akash rattan <akashrattan43@Gmail.com>
2020-12-14 22:44:25 -0800

    r

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ const App = () => {
           <Route path="/" component={HomeScreen} exact />
           <Route path="/product/:id" component={ProductScreen}/>
         </Container>
+        <h1>Tmrw forsure</h1>
       </main>
       <Footer />
     </Router>
